cd73939cad36b60827f8d0a84caf5c23ff5a2587
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-05-23 16:01:54 -0500

    AnalysisofErrorsforPhase000VersionOG

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,31 +1,44 @@
+import java.util.StringJoiner;
+
 public class TimeConversion {
 
 
 
 
-    /*
-     * Complete the 'timeConversion' function below.
-     *
+    /* Attempt OG Version 0000!
+     * HackerRank Solving & Analyzing OG Code Solution
+     * Instructions: Complete the 'timeConversion' function below.
      * The function is expected to return a STRING.
      * The function accepts STRING s as parameter.
      */
 
     public static String timeConversion(String s) {
-        // Write your code here
 
 
         String[] inputVals = s.split(":", 3);
 
-        inputVals[1] = hourValue
+        inputVals[1] = hourValue;
+        //TODO: L18 missing int value when referencing an array
+
+        int hourValue = Integer.parseInt(hourValue);
+
         int hourValue = Parse.int(hourValue);
+        //TODO: L23 Wrong format of Integer.parseInt() method - review special case
 
 
         for(String val : inputVals){
 
             if (val.equals("PM")){
 
-                if ((hourValue)) >= 1){
+
+                if (hourValue>= 1){
+                    // TODO: call in global variable without extra brackets
+
                     int convertNum  = Parse.int(hourValue) + 12;
+                    //TODO: Wrong use of Integer's parse method again.
+
+
+
                     inputVals[1] = String.valueOf(convertNum);
 
                     // hour value has been solved
@@ -33,11 +46,14 @@ public class TimeConversion {
                 }
 
 
-                while(inputVals.hasNext()){
+                //TODO: HasNext method can only be used with Scanner, while loop not needed here.
+
                     StringJoiner stringjoiner = new StringJoiner(":");
                     stringjoiner.add(val);
 
-                }
+                    // TODO: Review how to use Stringjoiner at end of program to return back a completed String
+
+
 
                 return //whatever StringJoiner generated
 
