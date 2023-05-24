3d604f04842d0db675b6ad5c015b547a1296c7a2
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-05-24 06:56:37 -0500

    3/24_MostUpTodate_OptimizedVersionOfCodeSolution_LessCode_LessComplicated_RemovedUseofStringJoinerAndStringSplitToUse_SubStringAndConcatMethods_Quicker

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,7 +1,11 @@
+import java.util.StringJoiner;
+
 public class TimeConversion {
 
 
 
+
+    // Day 3 of Studying / Working Solutions for Studying Math + Conversion Logic Problems.
 
     /*
      * Complete the 'timeConversion' function below.
@@ -13,43 +17,62 @@ public class TimeConversion {
     public static String timeConversion(String s) {
         // Write your code here
 
+        // 1. First write example input to create strategy around indices /format if possible
+        // Example Input: 07:05:45PM 7 indices before AM/PM
+        //                0123456789
+        int hour = Integer.parseInt(s.substring(0,2));
 
-        String[] inputVals = s.split(":", 3);
+        String minutes = s.substring(3,8); // check counting on this
 
-        inputVals[1] = hourValue
-        int hourValue = Parse.int(hourValue);
+        String dayTime = s.substring(8,10);
 
+        // 2. Bracket out & Alter EdgeCases(every number after 12 in pms) to perform number altering
+        // 2. It's not just numbers but time of day that should trigger alterations
 
-        for(String val : inputVals){
+        // TODO: What is the correct way to add in a ternary operator  statement
+        // TODO: * since I can't write "if (dayTime.equals("PM") && hour == 12)"  I wanted a shortcut
 
-            if (val.equals("PM")){
+        if (dayTime.equals("PM")){
 
-                if ((hourValue)) >= 1){
-                    int convertNum  = Parse.int(hourValue) + 12;
-                    inputVals[1] = String.valueOf(convertNum);
+           // TODO: Ternary Operator Attempt 2
+            (hour > 12) ? hour += 12;
 
-                    // hour value has been solved
-
-                }
-
-
-                while(inputVals.hasNext()){
-                    StringJoiner stringjoiner = new StringJoiner(":");
-                    stringjoiner.add(val);
-
-                }
-
-                return //whatever StringJoiner generated
-
-            }
-
+            // TODO: Ternary Operator Attempt 1
+            if (hour > 12 ) : hour += 12;
+        } else if (dayTime.equals("AM")) {
+            if (hour == 12) : hour = 0;
         }
 
+        String result = hour + minutes;
 
+
+        //3. Formatting Edits for Output -- delete Pm/AM
+        // in this case I opted out of
+
+        return result;
 
 
     }
 
+
+    /**
+     * Simplified and optimized code that splices the "tiem" as a String value by index
+     * @param s takes in an unknown String- one we can use the example for to count indices
+     * @return an updated Military Time of the provided Sttring
+     */
+
+
+    // TODO: Review In what UseCases is it helpful to Use Modulo % ?
+    // TODO: Tip from this Optimized Code - we can set a variable while doing math on it in 1 line
+
+    public static String timeConversionSolution(String s){
+
+            int hour = Integer.parseInt(s.substring(0, 2)) % 12;
+            if (s.endsWith("PM"))
+                hour += 12;
+            return String.format("%02d", hour) + s.substring(2, 8);
+
+    }
 
 
 
