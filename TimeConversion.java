1a4303b5920379be54a48974f85e7c3126e23379
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-05-24 06:17:24 -0500

    ApproachUsingSubStringMethod

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,5 +1,9 @@
+import java.sql.SQLOutput;
+
 public class TimeConversion {
 
+
+    // 07:05:45PM
 
 
 
@@ -11,47 +15,49 @@ public class TimeConversion {
      */
 
     public static String timeConversion(String s) {
-        // Write your code here
+        // Example input: 07:05:45PM -> 19:05:45
 
 
-        String[] inputVals = s.split(":", 3);
+        // 1. Identify the 2 dependents  * USE SUBSTRING()
 
-        inputVals[1] = hourValue
-        int hourValue = Parse.int(hourValue);
-
-
-        for(String val : inputVals){
-
-            if (val.equals("PM")){
-
-                if ((hourValue)) >= 1){
-                    int convertNum  = Parse.int(hourValue) + 12;
-                    inputVals[1] = String.valueOf(convertNum);
-
-                    // hour value has been solved
-
-                }
+        String hourNum = s.substring(0, 2);
+        String timeOfDay = s.substring(s.length() - 1, s.length() - 2);
 
 
-                while(inputVals.hasNext()){
-                    StringJoiner stringjoiner = new StringJoiner(":");
-                    stringjoiner.add(val);
+        // 2.  Only if the hour fed in, is after 12pm we will convert into military time
 
-                }
 
-                return //whatever StringJoiner generated
+        if (timeOfDay.equals("PM") && (Integer.parseInt(hourNum) < 12)) {
+            System.out.println("Time of Day is afternoon and military time is applied.");
 
-            }
+            int militaryTime = Integer.parseInt(hourNum) + (12);
+
+
+            String[] splitTime = s.split(":", 3);
+
+            // TODO: How can I look inside 'splittime's index to see what values lay there in hackerrank?
+
+            // Swap Values new Hour Time to Military
+            splitTime[1] = String.valueOf(militaryTime);
+
+        }
+
+        for (String element : splitTime) {
+
 
         }
 
 
+        // 12am = 00:00
+        // Naturally the first 12 hours in morning time (am are correct for military time)
+        // However, where it changes is at noon 12 pm - after 12pm these should be +1
 
 
+        // 2. Identify the time of day (Am/Pm)
+
+
+        return splitTime;
     }
-
-
-
 
 
     public static void main(String[] args) {
@@ -59,11 +65,6 @@ public class TimeConversion {
         timeConversion(s);
 
     }
-
-
-
-
-
 
 
 }
